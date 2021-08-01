918a610dc28007dc3e450738e842bbe64f1a80b3
Marcos Reis <mreis@inoa.com.br>
2021-08-01 12:38:49 -0300

    ajustes na geraarray

--- geradorArray.c
+++ geradorArray.c
@@ -3,22 +3,24 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define TAMANHO_ARRAY 100
+
 int main()
 {   
 
-    int arr[100];
+    int arr[TAMANHO_ARRAY];
     int i, j, temp;
     srand(time(NULL));
-    for(i = 0; i< 100; i++){
+    for(i = 0; i< TAMANHO_ARRAY; i++){
         arr[i] = i + 1;
     }
-    for(i = 0; i< 100; i++){
-        j = (rand()%9 ) + 1;
+    for(i = 0; i< TAMANHO_ARRAY; i++){
+        j = (rand()%(TAMANHO_ARRAY-1)) + 1;
         temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp; 
     }
-    for(i = 0; i< 100; i++){
+    for(i = 0; i< TAMANHO_ARRAY; i++){
         printf("  %d", arr[i]);
     }
     printf("\n");
